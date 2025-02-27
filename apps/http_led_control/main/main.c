72b02b005fc1dc50adb4edaf2c380822391490a3
Hagen Patzke <hagen.patzke@hu.nl>
2025-02-27 22:01:17 +0100

    add http_led_control app

---
+++ apps/http_led_control/main/main.c
@@ -0,0 +1,291 @@
+/* Simple HTTP LED control Server Example 
+
+   Derived from esp-idf\examples\protocols\http_server\simple
+   2025-02-27 Hagen Patzke
+ */
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <esp_log.h>
+#include <nvs_flash.h>
+#include <sys/param.h>
+#include "esp_netif.h"
+#include "protocol_examples_common.h"
+#include "protocol_examples_utils.h"
+#include "esp_tls_crypto.h"
+#include <esp_http_server.h>
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "esp_tls.h"
+#include "esp_check.h"
+
+#include "driver/gpio.h"
+
+#if !CONFIG_IDF_TARGET_LINUX
+#include <esp_wifi.h>
+#include <esp_system.h>
+#include "nvs_flash.h"
+#include "esp_eth.h"
+#endif  // !CONFIG_IDF_TARGET_LINUX
+
+#define EXAMPLE_HTTP_QUERY_KEY_MAX_LEN  (64)
+
+static const char *TAG = "http_led_control";
+
+
+static const uint8_t pinLED = CONFIG_EXAMPLE_PIN_LED;
+
+static void init_led_pin()
+{
+    esp_rom_gpio_pad_select_gpio(pinLED);
+    gpio_reset_pin((gpio_num_t)pinLED);
+    gpio_set_direction((gpio_num_t)pinLED, GPIO_MODE_OUTPUT);
+    gpio_set_level((gpio_num_t)pinLED, 0);
+    ESP_LOGI(TAG, "init_led_pin()");
+}
+
+static void set_led_pin(uint8_t value)
+{
+    gpio_set_level((gpio_num_t)pinLED, value ? 1 : 0);
+    ESP_LOGI(TAG, "set_led_pin(%d)", value);
+}
+
+
+/* HTTP GET handler (very simple version) */
+static esp_err_t led_get_handler(httpd_req_t *req)
+{
+    char*  buf;
+    size_t buf_len;
+
+    /* Get header value string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        ESP_RETURN_ON_FALSE(buf, ESP_ERR_NO_MEM, TAG, "buffer alloc failed");
+        /* Copy null terminated value string into buffer */
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found header => Host: %s", buf);
+        }
+        free(buf);
+    }
+
+    /* Read URL query string length and allocate memory for length + 1,
+     * extra byte for null termination */
+    buf_len = httpd_req_get_url_query_len(req) + 1;
+    if (buf_len > 1) {
+        buf = malloc(buf_len);
+        ESP_RETURN_ON_FALSE(buf, ESP_ERR_NO_MEM, TAG, "buffer alloc failed");
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+            ESP_LOGI(TAG, "Found URL query => %s", buf);
+            char param[EXAMPLE_HTTP_QUERY_KEY_MAX_LEN], dec_param[EXAMPLE_HTTP_QUERY_KEY_MAX_LEN] = {0};
+            /* Get value of expected key from query string */
+            if (httpd_query_key_value(buf, "value", param, sizeof(param)) == ESP_OK) {
+                ESP_LOGI(TAG, "Found URL query parameter => value=%s", param);
+                example_uri_decode(dec_param, param, strnlen(param, EXAMPLE_HTTP_QUERY_KEY_MAX_LEN));
+                ESP_LOGI(TAG, "Decoded query parameter => %s", dec_param);
+                /* Prepare set_led_pin(value) parameter */
+                uint8_t value = (uint8_t)dec_param[0];
+                if (value >= 48 && value < 58) {
+                    value -= 48; /* value = 0..9 */
+                    set_led_pin(value);
+                } else {
+                    ESP_LOGI(TAG, "led_get_handler: invalid 'value' parameter, LED not switched");
+                }                
+            }
+        }
+        free(buf);
+    }
+
+    /* Send response with custom headers and body set as the
+     * string passed in user context*/
+    const char* resp_str = (const char*) req->user_ctx;
+    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
+}
+
+static const httpd_uri_t led = {
+    .uri       = "/led",
+    .method    = HTTP_GET,
+    .handler   = led_get_handler,
+    /* Let's pass response string in user
+     * context to demonstrate it's usage */
+    .user_ctx  = "LED control GET processed OK"
+};
+
+/* HTTP POST handler */
+static esp_err_t led_post_handler(httpd_req_t *req)
+{
+    char buf[100];
+    int ret, remaining = req->content_len;
+
+    while (remaining > 0) {
+        /* Read the data for the request */
+        if ((ret = httpd_req_recv(req, buf,
+                        MIN(remaining, sizeof(buf)))) <= 0) {
+            if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+                /* Retry receiving if timeout occurred */
+                continue;
+            }
+            return ESP_FAIL;
+        }
+
+        /* Send back the same data */
+        httpd_resp_send_chunk(req, buf, ret);
+        remaining -= ret;
+
+        /* Log data received */
+        ESP_LOGI(TAG, "=========== RECEIVED DATA ==========");
+        ESP_LOGI(TAG, "%.*s", ret, buf);
+        ESP_LOGI(TAG, "====================================");
+
+        if (!strncmp(buf, "value=", 6)) {
+               /* Prepare set_led_pin(value) parameter */
+               uint8_t value = (uint8_t)buf[6];
+               if (value >= 48 && value < 58) {
+                   value -= 48; /* value = 0..9 */
+                   set_led_pin(value);
+               } else {
+                   ESP_LOGI(TAG, "led_post_handler: invalid 'value' parameter, LED not switched");
+               }                
+            }
+    }//end while
+
+    // End response
+    httpd_resp_send_chunk(req, NULL, 0);
+    return ESP_OK;
+}
+
+static const httpd_uri_t ledpost = {
+    .uri       = "/led",
+    .method    = HTTP_POST,
+    .handler   = led_post_handler,
+    .user_ctx  = NULL
+};
+
+/* HTTP_ANY handler */
+static esp_err_t any_handler(httpd_req_t *req)
+{
+    /* Send response with body set as the
+     * string passed in user context*/
+    const char* resp_str = (const char*) req->user_ctx;
+    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
+
+    // End response
+    httpd_resp_send_chunk(req, NULL, 0);
+    return ESP_OK;
+}
+
+static const httpd_uri_t any = {
+    .uri       = "/",
+    .method    = HTTP_ANY,
+    .handler   = any_handler,
+    /* Let's pass response string in user
+     * context to demonstrate it's usage */
+    .user_ctx  = "Welcome to http_led_control!"
+};
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+#if CONFIG_IDF_TARGET_LINUX
+    // Setting port as 8001 when building for Linux. Port 80 can be used only by a privileged user in linux.
+    // So when a unprivileged user tries to run the application, it throws bind error and the server is not started.
+    // Port 8001 can be used by an unprivileged user as well. So the application will not throw bind error and the
+    // server will be started.
+    config.server_port = 8001;
+#endif // !CONFIG_IDF_TARGET_LINUX
+    config.lru_purge_enable = true;
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &led);
+        httpd_register_uri_handler(server, &ledpost);
+        httpd_register_uri_handler(server, &any);
+        return server;
+    }
+
+    ESP_LOGI(TAG, "Error starting server!");
+    return NULL;
+}
+
+#if !CONFIG_IDF_TARGET_LINUX
+static esp_err_t stop_webserver(httpd_handle_t server)
+{
+    // Stop the httpd server
+    return httpd_stop(server);
+}
+
+static void disconnect_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server) {
+        ESP_LOGI(TAG, "Stopping webserver");
+        if (stop_webserver(*server) == ESP_OK) {
+            *server = NULL;
+        } else {
+            ESP_LOGE(TAG, "Failed to stop http server");
+        }
+    }
+}
+
+static void connect_handler(void* arg, esp_event_base_t event_base,
+                            int32_t event_id, void* event_data)
+{
+    httpd_handle_t* server = (httpd_handle_t*) arg;
+    if (*server == NULL) {
+        ESP_LOGI(TAG, "Starting webserver");
+        *server = start_webserver();
+    }
+}
+#endif // !CONFIG_IDF_TARGET_LINUX
+
+void app_main(void)
+{
+    static httpd_handle_t server = NULL;
+
+    ESP_ERROR_CHECK(nvs_flash_init());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+
+    /* Register event handlers to stop the server when Wi-Fi or Ethernet is disconnected,
+     * and re-start it upon connection.
+     */
+#if !CONFIG_IDF_TARGET_LINUX
+#ifdef CONFIG_EXAMPLE_CONNECT_WIFI
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+#endif // CONFIG_EXAMPLE_CONNECT_WIFI
+#ifdef CONFIG_EXAMPLE_CONNECT_ETHERNET
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &connect_handler, &server));
+    ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ETHERNET_EVENT_DISCONNECTED, &disconnect_handler, &server));
+#endif // CONFIG_EXAMPLE_CONNECT_ETHERNET
+#endif // !CONFIG_IDF_TARGET_LINUX
+
+    init_led_pin();
+    // Built-In Self Test: flash LED three times
+    for (uint8_t i = 1; i < 7; i++)
+    {
+        sleep(1);
+        set_led_pin(i & 1);
+    }
+
+    /* Start the server for the first time */
+    server = start_webserver();
+
+    while (server) {
+        sleep(5);
+    }
+}
